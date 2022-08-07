d49226ce7c8a7523d89bd27e6868fa10265daef5
Gabriel Rosa Corona <gabokuck1990@gmail.com>
2022-08-07 16:57:19 -0500

    correccion de importqación del http adapter

--- src/common/adapters/axios.adapter.ts
+++ src/common/adapters/axios.adapter.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
-import { HttpAdapter } from '../../../dist/common/interfaces/http-adapter.interface';
+import { HttpAdapter } from '../interfaces/http-adapter.interface';
+
 
 @Injectable()
 export class AxiosAdapter implements HttpAdapter {
